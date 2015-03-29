f37a03d49e4de346fc27637923e121def1f20563
v.audacity <v.audacity@47890b92-0858-11df-a26f-8b716316a5bc>
2015-03-29 10:50:21 +0000

    Gale's updates

--- website/trunk/htdocs/about/screenshots.php
+++ website/trunk/htdocs/about/screenshots.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * Copyright 2004 - 14 Matt Brubeck, Gale Andrews
+ * Copyright 2004 - 15 Matt Brubeck, Gale Andrews
  * This file is licensed under a Creative Commons license:
  * http://creativecommons.org/licenses/by/2.0/
  */
@@ -20,11 +20,11 @@
 
 <p class="screenshot"><a href="images/audacity-macosx.png"><img alt="" src="images/audacity-macosx-small.jpg"><?=_("Audacity running on Mac OS X")?></a></p>
 
-<p class="screenshot"><a href="images/effect-noise-removal.png"><img alt="" src="images/effect-noise-removal-small.jpg"><?=_("Noise Removal effect")?></a></p>
+<p class="screenshot"><a href="images/effect-noise-reduction.png"><img alt="" src="images/effect-noise-reduction-small.png"><?=_("Noise Reduction effect")?></a></p>
 
-<p class="screenshot"><a href="images/effect-change-pitch.png"><img alt="" src="images/effect-change-pitch-small.jpg"><?=_("Change Pitch effect")?></a></p>
+<p class="screenshot"><a href="images/effect-change-pitch.png"><img alt="" src="images/effect-change-pitch-small.png"><?=_("Change Pitch effect")?></a></p>
 
-<p class="screenshot"><a href="images/effect-change-tempo.png"><img alt="" src="images/effect-change-tempo-small.jpg"><?=_("Change Tempo effect")?></a></p>
+<p class="screenshot"><a href="images/effect-change-tempo.png"><img alt="" src="images/effect-change-tempo-small.png"><?=_("Change Tempo effect")?></a></p>
 
 <p class="screenshot"><a href="images/recording.png"><img alt="" src="images/recording-small.jpg"><?=_("Recording with Audacity")?></a></p>
 
