d9ebb709101e9b6243c0e92a99a537437e2fe63c
v.audacity@gmail.com <v.audacity@gmail.com@47890b92-0858-11df-a26f-8b716316a5bc>
2015-04-08 07:08:08 +0000

    Remove duplicate entry, now Paul Licameli is part of Audacity Team.

--- audacity-src/trunk/src/AboutDialog.cpp
+++ audacity-src/trunk/src/AboutDialog.cpp
@@ -99,7 +99,6 @@ void AboutDialog::CreateCreditsList()
    AddCredit(wxT("Steve Jolly"), roleContributor);
    AddCredit(wxT("Steven Jones"), roleContributor);
    AddCredit(wxT("Arun Kishore"), roleContributor);
-   AddCredit(wxT("Paul Licameli"), roleContributor);
    AddCredit(wxT("Paul Livesey"), roleContributor);
    AddCredit(wxT("Harvey Lubin"), roleContributor);
    AddCredit(wxT("Greg Mekkes"), roleContributor);
