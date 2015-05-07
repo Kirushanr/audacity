5cf442c0489ead46aede3b404a693aa95435adbb
buanzo@gmail.com <buanzo@gmail.com@47890b92-0858-11df-a26f-8b716316a5bc>
2015-05-07 18:17:18 +0000

    adding class file for NewsItem

---
+++ website/trunk/htdocs/include/newsitem.class.inc.php
@@ -0,0 +1,23 @@
+<?php
+class NewsItem {
+  var $id;
+  var $date;
+  var $title;
+  var $body;
+
+  // Constructor.
+  function NewsItem($date, $title, $body) {
+    $this->date = $date;
+    $this->title = $title;
+    $this->body = $body;
+  }
+
+  // Returns the date, in the preferred format for the current locale.
+  function dateStr() {
+    // i18n-hint: Controls how dates are formatted.
+    // See http://www.php.net/manual/function.strftime.php for details.
+    return locale_to_unicode(strftime(_("%B %d, %Y"), $this->date));
+  }
+}
+
+?>
